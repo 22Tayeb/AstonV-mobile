90d1e99a4631c076c2fa050ea363a4ba89f78f7a
22Tayeb <tayeb.kharbouch27@gmail.com>
2024-06-03 17:16:08 +0200

    back connection

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -2,24 +2,56 @@ import { Image, StyleSheet, Platform } from 'react-native';
 import React, { useState } from 'react';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { View, Text, TextInput, Button, Alert, } from 'react-native';
+import {AsyncStorage} from 'react-native';
 
 import { Link } from 'expo-router';
 import { useRouter } from 'expo-router'
 
 export default function LoginScreen() {
 
-    const router = useRouter();
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const [password, setPassword] = useState('');
 
-    const handleLogin = () => {
-    // Ajoutez ici la logique de validation et d'authentification
-    if (email === 'test@example.com' && password === 'password') {
-      Alert.alert('Connexion réussie');
-      router.navigate('/home'); // Rediriger vers la page d'accueil après la connexion réussie
-    } else {
-      Alert.alert('Email ou mot de passe incorrect');
+  const handleLogin = () => {
+    if (!email || !password) {
+      Alert.alert('Erreur', 'Veuillez remplir tous les champs.');
+      return;
     }
+
+    fetch('https://backend-astonvoyage.vercel.app/api/user/authenticate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        mail: email,
+        mdp: password,
+      }),
+    })
+      .then((response) => {
+        console.log(response)
+        if (!response.ok) {
+          throw new Error('Erreur de connexion');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        // Gérer la réponse de l'API, par exemple stocker le token JWT
+        console.log(data);
+        try {
+         AsyncStorage.setItem('nom', data.token);
+        AsyncStorage.setItem('prenom', data.nom);
+        AsyncStorage.setItem('mail', data.mail);
+        AsyncStorage.setItem('id', data.user._id);
+        } catch (error) {
+      console.error('erreur', error);
+        }
+      })
+      .catch((error) => {
+        console.error('NVEL Erreur:', error);
+        Alert.alert('Erreur', 'Erreur de connexion.');
+      });
+   
   };
 
     const newLocal = '';
@@ -44,7 +76,7 @@ export default function LoginScreen() {
         onChangeText={setPassword}
         secureTextEntry
       />
-      <Link style={styles.title} href="/home">Se connecter</Link>
+      <Button title="Se connecter" onPress={handleLogin} />
       <Link style={styles.subtitle} href={"/inscription"}>Pas encore de compte? Inscrivez-vous</Link>
     </View>
 
--- app/(tabs)/paiement.tsx
+++ app/(tabs)/paiement.tsx
@@ -1,52 +1,21 @@
 
 import { Link } from 'expo-router';
 import React, { useState } from 'react';
-import { View, Button, StyleSheet, Alert } from 'react-native';
+import { View, Button, StyleSheet, Alert, Text } from 'react-native';
+
 
 
 export default function paiementScreen() {
 
-  const [cardData, setCardData] = useState({});
-
-//   const handleCardInput = (formData) => {
-//     setCardData(formData);
-//   };
-
-//   const handlePay = () => {
-//     if () {
-//       Alert.alert('Paiement réussi', 'Votre paiement a été effectué avec succès.');
-//     } else {
-//       Alert.alert('Erreur', 'Veuillez vérifier les informations de votre carte.');
-//     }
-//   };
-
   return (
     <View style={styles.container}>
-      <CreditCardInput
-        requiresName
-        requiresCVC
-        validColor="black"
-        invalidColor="red"
-        placeholderColor="darkgray"
-        // onChange={handleCardInput}
-      />
-      <View style={styles.buttonContainer}>
-        <Link style={styles.buttonContainer}>Payer</Link>
-      </View>
+     <Text>Paiement</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#F5FCFF',
-  },
-  buttonContainer: {
-    marginTop: 20,
-  },
+  
 });
 
 export default paiementScreen;
